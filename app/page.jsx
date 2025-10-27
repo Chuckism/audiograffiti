3f2827ed0e8a5bcced3a648cb67a77d3f0c5520e
Chuckism <chucknealis@gmail.com>
2025-10-27 13:56:30 -0500

    Force dynamic rendering for homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 
 /* ============================ CONSTANTS ============================ */
